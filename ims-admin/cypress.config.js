1329aca3e4b8f17f44fca2d4ff10a3c0fff6b4da
Sultan <531213@student.fontys.nl>
2025-01-05 21:46:48 +0100

    testonceagain

--- ims-admin/cypress.config.js
+++ ims-admin/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://sultan-303.github.io/IMS-Admin',
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3001/IMS-Admin',
     supportFile: 'cypress/support/e2e.js',
     specPattern: 'cypress/e2e/**/*.cy.js',
     env: {
